439d6d1f4ad30c9b273ea5ae20a845beedda6c12
Matthew Van Doff <mvandoff@gmail.com>
2024-04-08 17:35:18 -0400

    Fix prettier tailwind plugin, set up nav buttons, show copied message

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,6 +1,15 @@
 module.exports = {
-  singleQuote: true,
-  useTabs: true,
-  printWidth: 120,
-  plugins: ["prettier-plugin-tailwindcss", "prettier-plugin-astro"],
+	singleQuote: true,
+	useTabs: true,
+	printWidth: 120,
+	plugins: ['prettier-plugin-astro', 'prettier-plugin-tailwindcss'],
+	// pluginSearchDirs: false,
+	overrides: [
+		{
+			files: '*.astro',
+			options: {
+				parser: 'astro',
+			},
+		},
+	],
 };
